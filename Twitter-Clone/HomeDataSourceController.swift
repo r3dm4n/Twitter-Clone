c08ace67eeb4ead139bb3837cc10575697acf6fb
Alexandru Corut <r3dm4n@me.com>
2017-05-03 08:50:02 +0300

    User Cell Design

--- Twitter-Clone/HomeDataSourceController.swift
+++ Twitter-Clone/HomeDataSourceController.swift
@@ -26,7 +26,7 @@ class HomeDataSourceController: DatasourceController {
 
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-        return CGSize(width: view.frame.width, height: 100)
+        return CGSize(width: view.frame.width, height: 50)
     }
     
 }
