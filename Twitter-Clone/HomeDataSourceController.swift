59f87ab9ade400c035c3ca16ef77b996eb81a9f5
Alexandru Corut <r3dm4n@me.com>
2017-05-03 07:35:10 +0300

    User Cell Subview Layout

--- Twitter-Clone/HomeDataSourceController.swift
+++ Twitter-Clone/HomeDataSourceController.swift
@@ -16,6 +16,10 @@ class HomeDataSourceController: DatasourceController {
         self.datasource = homeDataSource
     }
     
+    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 50)
     }
